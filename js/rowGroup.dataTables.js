e6791b9f26d6b3bb1b94dc3f2c3bf1baa0cc7340
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:13:37 +0000

    d2b1dea28a80633f7f8a6b4d320b8dc04bf409d0
    Sync to source repo @2b1dea28a80633f7f8a6b4d320b8dc04bf409d0

--- js/rowGroup.dataTables.js
+++ js/rowGroup.dataTables.js
@@ -28,10 +28,9 @@
 				require('datatables.net-dt')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.RowGroup ) {
 				require('datatables.net-rowgroup')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/rowGroup.dataTables.mjs
+++ js/rowGroup.dataTables.mjs
@@ -4,7 +4,7 @@
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-dt';
-import 'datatables.net-rowgroup';
+import RowGroup from 'datatables.net-rowgroup';
 
 
 
