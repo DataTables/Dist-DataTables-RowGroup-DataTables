cc7d6a9426280323fdb48a522870f98794187f51
dtbuild <dtbuild@rigil>
2026-07-22 14:46:27 +0000

    f8eae909be69bd3f000913ac3f9672a8d765f734 Release 2.0.0 ready
    Sync to source repo @f8eae909be69bd3f000913ac3f9672a8d765f734

--- js/rowGroup.dataTables.min.mjs
+++ js/rowGroup.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! RowGroup default styling 2.0.0-beta.1 for DataTables
+/*! RowGroup default styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import RowGroup from"datatables.net-rowgroup";export default DataTable;
